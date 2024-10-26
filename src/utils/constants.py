8c7fb6dae58464fd57f5e52f671f7dfbabf804dd
Auri06 <118132700+Auri06@users.noreply.github.com>
2024-10-25 20:14:28 -0400

    refact : Start of constants.py file

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,6 +1,6 @@
-rojo= "red"
-alto = 800
-ancho = 700
-verde = "green"
-gris = (200, 200, 200)
-blanco = (255, 255, 255)
+rojo_enemigo= (255, 0, 0)
+alto_ventana = 800
+ancho_ventana = 700
+verde_personaje = (54, 255, 51)
+gris_bloke = (200, 200, 200)
+blanco_mapa = (255, 255, 255)
